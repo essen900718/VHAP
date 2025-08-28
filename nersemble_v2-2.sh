094e0fb2783c0dcc90578332ef98844db1a725e3
Isadora <59260026+essen900718@users.noreply.github.com>
2025-08-28 16:23:05 +0800

    Create nersemble_v2-2.sh

---
+++ nersemble_v2-2.sh
@@ -0,0 +1,14 @@
+NERSET="data/nersemble"
+SUBJECT="043"
+SEQUENCES=$(cat sequence_list_new.txt)
+
+for SEQUENCE in $SEQUENCES; do
+  TRACK_OUTPUT_FOLDER="output/nersemble/${SUBJECT}_${SEQUENCE}"
+
+  #  / 2~5 1e4 / 10 5e4 / 16 1e5 --w.reg_tex_tv 5e4 
+  python vhap/track_nersemble_v2.py --data.root_folder ${NERSET} \
+    --exp.output_folder $TRACK_OUTPUT_FOLDER \
+    --data.subject $SUBJECT --data.sequence $SEQUENCE \
+    --data.n_downsample_rgb 4
+
+done
